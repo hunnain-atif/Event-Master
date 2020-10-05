519f401bc6fce14be883fa06785249de2f6c913f
Hunnain Atif <td-dev@outlook.com>
2020-10-05 00:18:54 -0400

    initialing api

---
+++ eventMaster-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '817d9c527137fa226ff1338d76146508'),
+    },
+  },
+});
---
+++ eventMaster-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '85e2d0ba-bfea-4a9c-a8e0-902776663593'
+};
